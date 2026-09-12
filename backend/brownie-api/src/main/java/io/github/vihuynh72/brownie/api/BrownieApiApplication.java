d64b58c709ef9febd1e05288f4859b7897a250d4
Vi <vihuynh27.work@gmail.com>
2026-09-11 20:50:23 -0700

    Update API documentation and dependencies; enhance document management endpoints

--- backend/brownie-api/src/main/java/io/github/vihuynh72/brownie/api/BrownieApiApplication.java
+++ backend/brownie-api/src/main/java/io/github/vihuynh72/brownie/api/BrownieApiApplication.java
@@ -1,10 +1,13 @@
 package io.github.vihuynh72.brownie.api;
 
 import io.github.vihuynh72.brownie.api.config.BrownieEnvironmentListener;
+import io.github.vihuynh72.brownie.storage.azure.AzureBlobStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AzureBlobStorageConfig.class)
 public class BrownieApiApplication {
 
     public static void main(String[] args) {
